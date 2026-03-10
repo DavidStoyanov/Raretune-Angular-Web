1a23c573c6879fc26d2dccd9cf2db5554ac17e4c
DavidStoyanov <david_st1@abv.bg>
2026-03-11 00:23:41 +0200

    Add service and model for albums

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NewAlbum } from './futures/albums/new-album/new-album';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'albums/new', component: NewAlbum },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { asapScheduler } from 'rxjs';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterModule],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
