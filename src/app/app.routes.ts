5e3b9f049b2b67b0e35f13a613a800ae9e2bafba
DavidStoyanov <david_st1@abv.bg>
2026-04-25 17:12:05 +0300

    Craete components for about ans faq pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { SongBoard, SongContent, SongCreate, SongEdit } from './features/songs/components';
 import { Login, Register, Profile } from './features/users/components';
+import { About, Faq } from './pages';
 
 export const routes: Routes = [
     { path: 'auth/login', component: Login },
@@ -11,4 +12,6 @@ export const routes: Routes = [
     { path: 'song/catalog', component: SongBoard },
     { path: 'song/:songId', component: SongContent },
     { path: 'song/:songId/edit', component: SongEdit },
+    { path: 'about', component: About },
+    { path: 'faq', component: Faq },
 ];
---
+++ src/app/pages/faq/faq.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-faq',
+  imports: [RouterModule],
+  templateUrl: './faq.html',
+  styleUrl: './faq.scss',
+})
+export class Faq {
+
+}
